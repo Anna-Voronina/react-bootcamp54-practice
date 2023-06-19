48c350a2b448dc34d7bd24d9e19588584fe04b6b
Anna-Voronina <anna.vs2512@gmail.com>
2023-06-19 15:30:08 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import { BlogCard } from './BlogCard/BlogCard';
-import article from 'data/article.json';
 import { Stats } from './Stats/Stats';
+import article from 'data/article.json';
 import data from 'data/data.json';
 
 export const App = () => {
   return (
     <>
       <BlogCard {...article} />
-      <Stats title="Main Statistics" stats={data} />{' '}
+      <Stats title="Main Statistics" stats={data} />
     </>
   );
 };
