8a91d30134cb9e087a65321601e894becd8086a9
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2023-06-26 13:59:40 +0200

    gallery

---
+++ src/tabs/Gallery.jsx
@@ -0,0 +1,8 @@
+import { ToDoForm } from "components/ToDoForm/ToDoForm";
+import { Component } from "react";
+
+export class Gallery extends Component {
+    render() {
+        return <ToDoForm/>
+   }
+}
