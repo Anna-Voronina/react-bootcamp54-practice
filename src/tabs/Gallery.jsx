781ac5f83f8e7fff543c313637965b0227dd6bf9
Vlad0si4 <boiko.vlad91@gmail.com>
2023-06-27 15:18:26 +0300

    task1

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,26 +1,55 @@
-import { ToDoForm } from "components/ToDoForm/ToDoForm";
-import { Component } from "react";
-import { getImages } from "service/image.Api";
+import { GalleryList } from 'components/GallaryList/GallaryList';
+import { ToDoForm } from 'components/ToDoForm/ToDoForm';
+import { Component } from 'react';
+import { getImages } from 'service/image.Api';
 
 export class Gallery extends Component {
-    state = {
-        page: 1,
-        query :''
+  state = {
+    page: 1,
+    query: '',
+    images: [],
+    showBtn: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { page, query } = this.state;
+    if (prevState.page !== page || prevState.query !== query) {
+      getImages(query, page).then(({ photos, total_results }) => {
+        console.log(photos);
+        this.setState(prevState => ({
+          images: [...prevState.images, ...photos],
+          showBtn: page < Math.ceil(total_results / 15),
+        }));
+      });
     }
-    
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-        const { page, query } = this.state;
-        if (prevState.page !== page || prevState.query !== query) {
-        getImages(query,page)
-    }
-}
+  handelSubmit = query => {
+    this.setState({
+      query: query,
+      page: 1,
+      images: [],
+      showBtn: false,
+    });
+  };
+  onClickButton = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
 
-    handelSubmit = (query) => {
-        this.setState({query: query})  
-}
-
-    render() {
-        return <ToDoForm onSubmit={this.handelSubmit} />
-   }
+  render() {
+    const { images, showBtn } = this.state;
+    return (
+      <>
+        <ToDoForm onSubmit={this.handelSubmit} />
+        <GalleryList images={images} />
+        {showBtn && (
+          <button onClick={this.onClickButton} type="button">
+            Load more
+          </button>
+        )}
+      </>
+    );
+  }
 }
