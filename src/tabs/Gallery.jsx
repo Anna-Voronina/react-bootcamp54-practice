57e66b1f12d36a42db4c9cf03964798f81585336
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-06-29 14:40:11 +0300

    modal task

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -2,12 +2,15 @@ import { GalleryList } from 'components/GallaryList/GallaryList';
 import { ToDoForm } from 'components/ToDoForm/ToDoForm';
 import { getImages } from 'service/image.Api';
 import { useState, useEffect } from 'react';
+import { Modal } from 'components/Modal/Modal';
 
 export const Gallery = () => {
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
   const [showBtn, setShowBtn] = useState(false);
+  const [largeImg, setLargeImg] = useState('');
+  const [altImg, setAltImg] = useState('');
 
   useEffect(() => {
     if (!query) return;
@@ -28,14 +31,27 @@ export const Gallery = () => {
     setPage(prevState => prevState + 1);
   };
 
+  const openModal = ({ large, alt }) => {
+    setLargeImg(large);
+    setAltImg(alt);
+  };
+
+  const closeModal = () => {
+    setLargeImg('');
+    setAltImg('');
+  };
+
   return (
     <>
       <ToDoForm onSubmit={handelSubmit} />
-      <GalleryList images={images} />
+      <GalleryList images={images} openModal={openModal} />
       {showBtn && (
         <button onClick={onClickButton} type="button">
           Load more
         </button>
+      )}
+      {largeImg && (
+        <Modal largeImg={largeImg} altImg={altImg} closeModal={closeModal} />
       )}
     </>
   );
