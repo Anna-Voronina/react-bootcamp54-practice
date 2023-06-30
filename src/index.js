0973abca7885159fd787cc0112593ada708efc54
Vyachuk <Slavkoka045@gmail.com>
2023-06-30 15:13:23 +0300

    added useMemo

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import { App } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/theme';
-import GlobalCSS from 'styles/global.styles'
-
+import GlobalCSS from 'styles/global.styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
+  // <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-    <GlobalCSS/>
-    </ThemeProvider>
-  </React.StrictMode>
+    <GlobalCSS />
+  </ThemeProvider>
+  // </React.StrictMode>
 );
